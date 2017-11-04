16dd82b865855129050bf12c015033fcb92f78e1
Kenji-H <kenji1226h@gmail.com>
2017-11-05 02:55:00 +0900

    remove parens from ChannelPipeline.last()

--- src/main/scala/com/kenjih/example/factorial/FactorialClient.scala
+++ src/main/scala/com/kenjih/example/factorial/FactorialClient.scala
@@ -17,7 +17,7 @@ object FactorialClient {
         .channel(classOf[NioSocketChannel])
         .handler(new FactorialClientInitializer())
       val f       = b.connect(HOST, PORT).sync()
-      val handler = f.channel.pipeline.last().asInstanceOf[FactorialClientHandler]
+      val handler = f.channel.pipeline.last.asInstanceOf[FactorialClientHandler]
       println(s"Factorial of $COUNT is ${handler.getFactorial}")
     } finally {
       group.shutdownGracefully()
